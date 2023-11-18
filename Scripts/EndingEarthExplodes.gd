adb7026d2bd9db39afe890684b90954cbd166ac4
courtesyy <62979781+courtesyy@users.noreply.github.com>
2023-11-18 13:46:09 -0800

    Earth explosion

---
+++ Scripts/EndingEarthExplodes.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+func _ready():
+	$AnimationPlayer.play("EarthExplosion")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
