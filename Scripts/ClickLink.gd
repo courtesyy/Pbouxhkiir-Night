c330012897907e4aba33acdc2490b4be5a5465db
pc <61981562+cep450@users.noreply.github.com>
2023-11-19 01:19:04 -0500

    splash screen appears in series of clicks and starts ink

--- Scripts/ClickLink.gd
+++ Scripts/ClickLink.gd
@@ -1,16 +1,44 @@
-extends Node
+extends Node2D
 
+# when the player clicks anywhere, display something else. 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var clicked = false; 
+export (bool)var listeningForClicks = false
+export (bool)var shouldStartInvisible = false
+#export (NodePath)onready var nextNode = get_node(nextNode) as Node2D
+export (NodePath) var nextNodePath
+var nextNode
 
+var secondsBeforeClickable = 0.25;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if(shouldStartInvisible):
+		hide()
+	nextNode = get_node(nextNodePath)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+
+
+	if(!listeningForClicks):
+		return
+
+	#if clickable, wait for clickable 
+	if(secondsBeforeClickable > 0.0):
+		secondsBeforeClickable = secondsBeforeClickable - delta
+		return
+
+
+# reveive the signal from the proceed button 
+func receiveClick():
+
+	if(!listeningForClicks || secondsBeforeClickable > 0.0):
+		return
+
+	listeningForClicks = false;
+	nextNode.show()
+	nextNode.listeningForClicks = true 
+
+
--- Scripts/splashscreen.gd
+++ Scripts/splashscreen.gd
@@ -1,10 +1,22 @@
 extends Node2D
 
 
+var listeningForClicks = false;
 
 func _ready():
 	pass
 
 
 func _on_Proceed_pressed():
-	emit_signal("dialogue_done")
+	#emit_signal("dialogue_done")
+	if(listeningForClicks):
+		
+		print_debug("starting ink")
+
+		#TODO instantiate the ink player, or emit a signal, or something 
+
+
+		return
+	pass
+
+
